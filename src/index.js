63c9aee69273fe5efd216701f4cba911d0f27b22
Marcelinovitorino <168681717+Marcelinovitorino@users.noreply.github.com>
2025-05-01 14:10:52 -0800

    adcionando o cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import userRoutes from './routes/user.js';
 import multerConfig  from './config/multerConfig.js'
+import cors from 'cors'
 
 const app = express();
 
 
 app.use(express.json());
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static('uploads')); 
 
